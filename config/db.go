298823dd07fd4fa53e9673b337d304df7840a3bb
DVTcode <vinhtoanok@gmail.com>
2025-07-09 10:31:50 +0700

    Resolve conflicts after stash pop – keep local changes

--- config/db.go
+++ config/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/DVTcode/podcast_server/models"
 	"github.com/joho/godotenv"
@@ -23,20 +24,20 @@ func LoadEnv() {
 func ConnectDB() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True",
 		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
+		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
 
 	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}) //mở kết nối đến DB qua driver mysql.
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}) //mở kết nối đến DB qua driver mysql.
 	// gor.Open Hàm của GORM để mở kết nối CSDL. Trả về *gorm.DB và error.
 	// &gorm.Config{} Cấu hình thêm cho GORM (bạn có thể để trống hoặc tùy chỉnh sâu hơn).
 	if err != nil {
 		log.Fatal("❌ Failed to connect DB:", err)
 	}
-
+	DB = db
 	// ✅ Auto migrate các bảng
 	err = DB.AutoMigrate(
 		&models.NguoiDung{},
@@ -49,4 +50,15 @@ func ConnectDB() {
 	}
 
 	fmt.Println("✅ Connected to DB and Migrated!")
+
+	// Cài đặt trước của Connection pool
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Không thể lấy dữ được đối tượng cơ sở dữ liệu: %v", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)  // số kết nối rảnh tối đa (không dùng nhưng giữ lại)
+	sqlDB.SetMaxOpenConns(100) // tổng số kết nối mở tối đa
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+
 }
--- models/category.go
+++ models/category.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type DanhMuc struct {
-	ID         string    `gorm:"type:char(36);primaryKey" json:"id"`
+	ID         string    `gorm:"type:char(36);primaryKey;default:(UUID())" json:"id"`
 	TenDanhMuc string    `gorm:"type:varchar(255)" json:"ten_danh_muc"`
 	MoTa       string    `gorm:"type:text" json:"mo_ta"`
 	Slug       string    `gorm:"type:varchar(100);uniqueIndex" json:"slug"`
